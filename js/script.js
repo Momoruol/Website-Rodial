0389dd99f897c118686415325fb1d83f82ecf576
Joao Eduardo Notebook Momo <dudu280325@gmail.com>
2026-04-18 13:45:38 -0300

    atualização e semiconclusão da capa do website, ficando apenas as imagens dos cards para ser feito

--- js/script.js
+++ js/script.js
@@ -1,1 +1,9 @@
-// Add custom JavaScript here
+const cadeado = document.querySelector('#restrita')
+
+function abre_cadeado_hover(){
+    cadeado.querySelector('.bi').classList.toggle('bi-unlock2')
+    cadeado.querySelector('.bi').classList.toggle('bi-lock')
+}
+
+cadeado.addEventListener('mouseenter', abre_cadeado_hover)
+cadeado.addEventListener('mouseleave', abre_cadeado_hover)
